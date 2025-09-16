3cedd9669fb9d9f57d597ce4903945df13ab3d1f
agusmeritens22 <82111812+agusmeritens22@users.noreply.github.com>
2025-09-16 20:00:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,24 @@
-const CACHE = 'rutas-timewin-v7';
+const CACHE = "rutas-v9";
 const FILES = [
-  './',
-  './index.html',
-  './app.js?v=6',
-  // Si TENÉS manifest e íconos, dejalos. Si no, quitalos o subí los archivos.
-  // './manifest.webmanifest?v=6',
-  // './icons/icon-192.png',
-  // './icons/icon-512.png'
+  "./",
+  "./index.html",
+  "./app.js?v=9"
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e)=>{
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => k !== CACHE ? caches.delete(k) : null)
-    ))
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e)=>{
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))
   );
 });
